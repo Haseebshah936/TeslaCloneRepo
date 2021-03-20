2ba8f58c4f562acecf52aba64d64899bebcc1a17
Haseeb Shah <haseebshah936@gmail.com>
2021-03-20 18:37:13 +0500

    Rewerting change

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { ImageBackground, Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import CarsList from './Components/CarsList';
+import Header from './Components/Header'
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+        <Header />
+        <CarsList/>
+
+      <StatusBar style={'auto'} hidden={true} showHideTransition={'fade','slide'}/>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    // paddingTop: Platform.OS==="android" ? 20 : 0
+  },
+});
